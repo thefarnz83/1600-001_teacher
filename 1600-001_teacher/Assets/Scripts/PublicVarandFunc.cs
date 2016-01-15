ab94f4171133216904aa974b0dc4cef7e301e837
Michael Farnsworth <thefarnz83@gmail.com>
2016-01-15 09:36:13 -0700

    Adding First Scripts

---
+++ 1600-001_teacher/Assets/Scripts/PublicVarandFunc.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PublicVarandFunc : MonoBehaviour 
+{
+	public int myInt = 2;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		myInt = MultiplyByTwo(myInt);
+		Debug.Log (myInt);
+	}
+
+	int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+}
---
+++ 1600-001_teacher/Assets/Scripts/VarandFunc.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VarandFunc : MonoBehaviour 
+{
+	int myInt = 10;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myInt = DivideByTwo(myInt);
+		Debug.Log (myInt);
+	}
+
+	int DivideByTwo (int number)
+	{
+		int ret;
+		ret = number / 2;
+		return ret;
+	}
+}
