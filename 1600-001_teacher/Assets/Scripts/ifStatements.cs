04473c2478dd56a6eaffc9839d8fe74d6f1d990f
Michael Farnsworth <thefarnz83@gmail.com>
2016-01-20 08:20:31 -0700

    Adding if Statement script and scene

---
+++ 1600-001_teacher/Assets/Scripts/ifStatements.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifStatements : MonoBehaviour 
+{
+	//This is my script that I am going to committttttttt
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
