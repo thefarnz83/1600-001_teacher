ccdf3d02a7301d30449ce19534f14d95e9c86d46
Michael Farnsworth <thefarnz83@gmail.com>
2016-02-22 08:17:32 -0700

    pull request scene and script

--- 1600-001_teacher/Assets/NewBehaviourScript.cs
+++ 1600-001_teacher/Assets/NewBehaviourScript.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		Debug.Log ("Hello, this is a test of the pull requests. This does nothing");
 	}
 	
 	// Update is called once per frame
